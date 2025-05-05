3da00908a9926e94be78cd4a74cdf955c15bbc2e
shu07002 <shu07002@gmail.com>
2025-05-05 23:42:22 +0900

    feat: refactor post submission to use async/await and improve loading state management

--- src/pages/WritePost.jsx
+++ src/pages/WritePost.jsx
@@ -3,11 +3,14 @@ import TitleSection from "../components/WritePost/TitleSection";
 import ContentSection from "../components/WritePost/ContentSection";
 import WriteButton from "../components/WritePost/WriteButton";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function WritePost() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const onChangeTitle = (e) => {
     setTitle(e.target.value.trim());
@@ -17,37 +20,39 @@ export default function WritePost() {
     setContent(e.target.value.trim());
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    fetch(`${import.meta.env.VITE_API_URL}/articles`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-      body: JSON.stringify({
-        title,
-        content,
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("something went wrong");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Post created:", data);
-        setTitle("");
-        setContent("");
-        // ✅ 여기서 redirect를 해줘야 함
-        // 글 리스트로 이동
-        // 뒤로 가기 막기
-      })
-      .catch((error) => {
-        console.error("Error creating post:", error);
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/articles`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // ✅ 일다 여기 토큰 네임 임시
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+        body: JSON.stringify({
+          title,
+          content,
+        }),
       });
+
+      if (!response.ok) {
+        throw new Error("something went wrong");
+      }
+
+      await response.json();
+      //console.log("Post created:", data);
+
+      navigate("/");
+    } catch (error) {
+      console.error("Error creating post:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
